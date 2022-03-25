e7fa9ede28f76af7bcef7e6a2c779dfbc49ca260
clem <clemens.cords@aol.com>
2022-03-25 23:13:43 +0100

    compression working

--- non_obfuscated/shared_library_utils.jl
+++ non_obfuscated/shared_library_utils.jl
@@ -10,21 +10,9 @@ module Vi
     const hair = Char(0x200A)
     const carriage_return = Char(0x0D)
 
-    const digit_mapping = Dict([
-
-        0 => Char(33),
-        1 => Char(34),
-        2 => Char(35),
-        3 => Char(36),
-        4 => Char(37),
-        5 => Char(38),
-        6 => Char(39),
-        7 => Char(40),
-        8 => Char(41),
-        9 => Char(42)
-    ]);
-
-    const digit_unmapping = Dict(collect(pair.second => pair.first for pair in Vi.digit_mapping))
+    const digit_mapping_first_i = 48;#33;
+    const digit_mapping = Dict([i-digit_mapping_first_i => Char(i) for i in digit_mapping_first_i:(digit_mapping_first_i + 10)])
+    const digit_unmapping = Dict([pair.second => pair.first for pair in Vi.digit_mapping])
 
     function encode_number(count::Int64) ::String
 
@@ -53,6 +41,69 @@ module Vi
         return out;
     end
 
+    function zip(input::Vector{Char}) ::Vector{Char}
+
+        out = Vector{Char}()
+        sizehint!(out, length(input))
+
+        origin_i = 1
+        while origin_i <= length(input)
+
+            current = input[origin_i]
+            push!(out, current)
+
+            count = 0
+            while origin_i+1 < length(input) && input[origin_i+1] == current
+                count += 1
+                origin_i += 1
+            end
+
+            if count > 2
+                number = encode_number(count)
+                for c in number
+                    push!(out, c)
+                end
+            end
+
+           origin_i += 1
+        end
+
+        return out
+    end
+
+    function unzip(str_in::Vector{Char}) ::Vector{Char}
+
+        out = Vector{Char}()
+
+        i = 1
+        while i <= length(str_in)
+
+            current = str_in[i]
+            count = 1
+            offset = 1
+            if haskey(digit_unmapping, current)
+                number = string(current)
+                while haskey(digit_unmapping, str_in[i+offset])
+                    number *= string(str_in[i+offset])
+                    offset += 1
+                end
+                count = decode_number(number)
+            end
+
+            if (count > 1)
+                for _ in 1:count
+                    push!(out, str_in[i - 1])
+                end
+            else
+                push!(out, current)
+            end
+
+            i += offset;
+        end
+
+        return out
+    end
+
 
     function encode(binary_path::String = "...") ::String
 
@@ -60,7 +111,7 @@ module Vi
         pipe = Base64EncodePipe(io);
         write(pipe, open(binary_path))
 
-        as_uint8 = Vector{UInt8}(take!(io))
+        as_uint8 = Vector{Char}(take!(io))
 
         encoded = Vector{Char}()
         zero = zero_width
@@ -111,5 +162,12 @@ n = 1234567890
 println(n)
 println(Vi.encode_number(n))
 println(Vi.decode_number(encode_number(n)))
+
+str = "AAAAAAAAABCDEFGHIJKLMNOPPPPPPQRSTUUUUV"
+zipped = zip([i for i in str])
+unzipped = unzip(zipped);
+println(str)
+println(String(zipped))
+println(String(unzipped))
 
 end
